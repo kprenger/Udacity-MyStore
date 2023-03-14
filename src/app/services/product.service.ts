e12329df036c4a490410d08abd3677dd4dd505cd
Kurt Prenger <kprenger@its.jnj.com>
2023-03-13 21:39:50 -0400

    Updating product service to connect to backend API

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
-import { map } from 'rxjs/operators'
 import { HttpClient } from '@angular/common/http'
 
 import { Product } from '../models/product'
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +12,10 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>('../../assets/data.json')
+    return this.http.get<Product[]>(`${environment.apiUrl}/api/products`)
   }
 
-  getProduct(id: number | string): Observable<Product | undefined> {
-    return this.getProducts().pipe(
-      map((products: Product[]) => {
-        const idToCheck: number = typeof id === 'string' ? parseInt(id) : id
-        return products.find((product) => product.id === idToCheck)
-      })
-    )
+  getProduct(id: number | string): Observable<Product> {
+    return this.http.get<Product>(`${environment.apiUrl}/api/products/${id}`)
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: 'http://localhost:3000',
+  jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJmaXJzdE5hbWUiOiJEZWZhdWx0IiwibGFzdE5hbWUiOiJVc2VyIiwidXNlcm5hbWUiOiJkZWZVc2VyIn0sImlhdCI6MTY3Nzg3ODExNX0.EjieaKdyHOxYmEcTebdANN9qHXczPbE-nd2BPe0x8-I'
+}
