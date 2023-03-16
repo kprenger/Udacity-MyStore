8b3fcdfa7e89f61bbec6bfe8636acb38ba17c922
Kurt Prenger <kprenger@its.jnj.com>
2023-03-16 16:36:28 -0400

    Updating environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000',
-  jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJmaXJzdE5hbWUiOiJEZWZhdWx0IiwibGFzdE5hbWUiOiJVc2VyIiwidXNlcm5hbWUiOiJkZWZVc2VyIn0sImlhdCI6MTY3Nzg3ODExNX0.EjieaKdyHOxYmEcTebdANN9qHXczPbE-nd2BPe0x8-I'
+  apiUrl:
+    'http://kp-udacity-fullstack-backend-dev.us-east-1.elasticbeanstalk.com',
+  jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJmaXJzdE5hbWUiOiJEZWZhdWx0IiwibGFzdE5hbWUiOiJVc2VyIiwidXNlcm5hbWUiOiJkZWZVc2VyIn0sImlhdCI6MTY3ODk5MjIzM30.IR6EFawq02mZNjax1X4iGvA5rL9nmtJO3dkwo1osNss'
 }
